318c3c194f29719c5531da05c41428f856239f3f
ravilella1234 <ravilella1234>
2019-11-22 06:02:46 +0530

    reportings

--- November5AMBatch/src/com/icici/loans/carloans/BaseTest.java
+++ November5AMBatch/src/com/icici/loans/carloans/BaseTest.java
@@ -3,11 +3,15 @@ package com.icici.loans.carloans;
 import java.io.FileInputStream;
 import java.util.Properties;
 
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
 
 public class BaseTest 
 {
@@ -16,6 +20,8 @@ public class BaseTest
 	static FileInputStream fis;
 	public static Properties p;
 	public static Properties or;
+	public static ExtentReports report= ExtentManager.getInstance();
+	public static ExtentTest test;
 	
 	public static void init() throws Exception
 	{
@@ -26,6 +32,8 @@ public class BaseTest
 		fis=new FileInputStream(projectPath+"//or.properties");
 		or=new Properties();
 		or.load(fis);
+		
+		PropertyConfigurator.configure(projectPath+"//log4j.properties");
 		
 	}
 	
