eda14d81b67f739e093848f6c9fe2c356a7af8a9
ravilella1234 <masters.selenium@gmail.com>
2019-12-05 09:08:24 +0530

    Excel Data reader using dataproviders & HashTable

---
+++ November5AMMavenRepo/src/main/java/com/excelDataReader/DoubleArray.java
@@ -0,0 +1,33 @@
+package com.excelDataReader;
+
+public class DoubleArray 
+{
+
+	public static void main(String[] args) 
+	{
+		/*
+		 * int[] a=new int[3]; a[0]=10; a[1]=20; a[2]=30; for(int i=0;i<a.length;i++) {
+		 * System.out.println(a[i]); }
+		 */
+		
+		
+		Object[][] a1=new Object[2][2];
+		a1[0][0]=10;
+		a1[0][1]=20;
+		a1[1][0]=30;
+		a1[1][1]=40;
+		
+		for(int i=0;i<2;i++)
+		{
+			for(int j=0;j<2;j++)
+			{
+				System.out.print(a1[i][j]);
+				System.out.print("\t");
+			}
+			System.out.println();
+		}
+					
+		
+	}
+
+}
---
+++ November5AMMavenRepo/src/main/java/com/excelDataReader/TC_003.java
@@ -0,0 +1,85 @@
+package com.excelDataReader;
+
+import org.testng.annotations.Test;
+
+import java.util.Hashtable;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.DataProvider;
+
+public class TC_003 
+{
+	WebDriver driver;
+	
+  @Test(dataProvider = "dp")
+  public void f(Hashtable<String, String> data) 
+  {
+	  if(data.get("RunMode").equals("y"))
+		  System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"//drivers//chromedriver.exe"); 
+	  		driver=new ChromeDriver();
+			driver.manage().window().maximize(); 
+			
+			driver.get("https://www.facebook.com");	
+			driver.findElement(By.id("email")).sendKeys(data.get("col2"));
+			driver.findElement(By.id("pass")).sendKeys(data.get("col3"));
+  }
+
+  @DataProvider
+  public Object[][] dp() throws Exception 
+  {
+	  ExcelAPI e=new ExcelAPI("C:\\Users\\DELL\\Desktop\\SuiteA.xlsx");
+		String sheetName="data";
+		String testCaseName="LoginTest";
+		
+		int testStartRowNum=0;
+		while(!e.getCellData(sheetName, 0, testStartRowNum).equals(testCaseName)) 
+		{
+			testStartRowNum++;
+		}
+		System.out.println("Test Starts from row :-  " + testStartRowNum);
+		
+		
+		int colStartRowNum=testStartRowNum+1;
+		int dataStartRowNum=testStartRowNum+2;
+		
+		//calculate rows of data
+		int rows=0;
+		while(!e.getCellData(sheetName, 0, dataStartRowNum+rows).equals("")) 
+		{
+			rows++;
+		}
+		System.out.println("Total rows are :-  "+ rows);
+		
+		//Calculate total Columns
+		int cols=0;
+		while(!e.getCellData(sheetName, cols, colStartRowNum).equals("")) 
+		{
+			cols++;
+		}
+		System.out.println("Total Cols are :-  "+ cols);
+		
+		
+		//read the data
+		int dataRow=0;
+		Object[][] data=new Object[rows][1];
+		
+		Hashtable<String,String> table=null;
+		for(int rNum=dataStartRowNum;rNum<dataStartRowNum+rows;rNum++)
+		{
+			table=new Hashtable<String,String>();
+			for(int cNum=0;cNum<cols;cNum++) 
+			{
+				//System.out.println(e.getCellData(sheetName, cNum, rNum));
+				//data[dataRow][cNum]=e.getCellData(sheetName, cNum, rNum);
+				String key=e.getCellData(sheetName, cNum, colStartRowNum);
+				String value=e.getCellData(sheetName, cNum, rNum);
+				table.put(key, value);
+			}	
+			data[dataRow][0]=table;
+			dataRow++;
+		}
+		return data;	 
+  }
+}
